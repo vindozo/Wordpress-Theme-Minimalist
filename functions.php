c1161debd147c4bbeca253e6083d695576f2fbf7
vindozo <vindozo@gmail.com>
2020-05-06 18:14:55 +0500

    Update functions.php
    restore API REST

--- functions.php
+++ functions.php
@@ -30,20 +30,5 @@ remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
 remove_action( 'wp_head', 'wp_generator' );
 remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
 remove_action( 'wp_head', 'wp_oembed_add_host_js' );
-remove_action( 'xmlrpc_rsd_apis', 'rest_output_rsd' );
 remove_action( 'wp_head', 'rest_output_link_wp_head', 10, 0 );
-remove_action( 'template_redirect', 'rest_output_link_header', 11, 0 );
-remove_action( 'auth_cookie_malformed', 'rest_cookie_collect_status' );
-remove_action( 'auth_cookie_expired', 'rest_cookie_collect_status' );
-remove_action( 'auth_cookie_bad_username', 'rest_cookie_collect_status' );
-remove_action( 'auth_cookie_bad_hash', 'rest_cookie_collect_status' );
-remove_action( 'auth_cookie_valid', 'rest_cookie_collect_status' );
-remove_action( 'init', 'rest_api_init' );
-remove_action( 'rest_api_init', 'rest_api_default_filters', 10, 1 );
-remove_action( 'parse_request', 'rest_api_loaded' );
-remove_action( 'rest_api_init', 'wp_oembed_register_route'              );
 
-remove_filter( 'rest_authentication_errors', 'rest_cookie_check_errors', 100 );
-remove_filter( 'rest_pre_serve_request', '_oembed_rest_pre_serve_request', 10, 4 );
-
-add_filter('rest_enabled', '__return_false');
